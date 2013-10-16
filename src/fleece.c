2999552c41b19ef4885993d4cd788f2857d7c1de
Aurelien ROUGEMONT <beorn@binaries.fr>
2013-10-16 10:21:28 +0200

    Fix memory leak and replace \t with whitespaces
     * was freeing pointer to json struct without freeing its contained data
     * replace \t with 4 whitespaces

--- src/fleece.c
+++ src/fleece.c
@@ -198,11 +198,11 @@ int main(int argc, char**argv)
     getopt_options[i].name = NULL;
 
     while (i = -1,
-	c = getopt_long_only(argc, argv, "+hv", getopt_options, &i), c != -1) {
+    c = getopt_long_only(argc, argv, "+hv", getopt_options, &i), c != -1) {
       switch (c) {
         case opt_version:
           printf("Fleece version %s\n",FLEECE_VERSION);
-	  return 0;
+      return 0;
         case opt_help:
           usage(argv[0]);
           return 0;
@@ -228,7 +228,7 @@ int main(int argc, char**argv)
           }
           extra_fields_len += 1;
           extra_fields = realloc(extra_fields,
-		                extra_fields_len * sizeof(struct kv));
+                        extra_fields_len * sizeof(struct kv));
           *tmp = '\0'; /* turn '=' into null terminator */
           tmp++; /* skip to first char of value */
           extra_fields[extra_fields_len - 1].key = strdup(optarg);
@@ -237,7 +237,7 @@ int main(int argc, char**argv)
           extra_fields[extra_fields_len - 1].value_len = strlen(tmp);
           break;
         default:
-	  printf("Not handled\n");
+      printf("Not handled\n");
           usage(argv[0]);
           return 1;
       }
@@ -297,34 +297,34 @@ int main(int argc, char**argv)
             }
        } else {
            /* hey there's data let's process it */
-	   jsonevent = json_loads(sendline, 0, &jsonerror);
+       jsonevent = json_loads(sendline, 0, &jsonerror);
            if (jsonevent== NULL)
-	   {
-		/* json not parsed ok then push jsonified version of msg */
-	   	jsonevent = json_object();
-		json_object_set(jsonevent, "message", json_string(sendline));
+       {
+        /* json not parsed ok then push jsonified version of msg */
+           jsonevent = json_object();
+        json_object_set(jsonevent, "message", json_string(sendline));
            }
            /* json parsed ok */
-	   for ( j = 0; j < extra_fields_len; j++)
-	   {
-	   	json_object_set(jsonevent, extra_fields[j].key, \
-	   		 json_string(extra_fields[j].value));
-	   }
+       for ( j = 0; j < extra_fields_len; j++)
+       {
+           json_object_set(jsonevent, extra_fields[j].key, \
+                json_string(extra_fields[j].value));
+       }
 
             /* add mandatory fields */
-	   json_object_set(jsonevent, "file", json_string("-"));
-	   json_object_set(jsonevent, "host", json_string(hostname));
+       json_object_set(jsonevent, "file", json_string("-"));
+       json_object_set(jsonevent, "host", json_string(hostname));
 
-	   /* copy modified json string to sendline */
-	   jsoneventstring = json_dumps(jsonevent,JSON_COMPACT);
-	   strcpy(sendline, jsoneventstring);
+       /* copy modified json string to sendline */
+       jsoneventstring = json_dumps(jsonevent,JSON_COMPACT);
+       strcpy(sendline, jsoneventstring);
 
-	   sendto(sockfd, sendline, strlen(sendline), 0, \
-	   	(struct sockaddr *)&servaddr, sizeof(servaddr));
+       sendto(sockfd, sendline, strlen(sendline), 0, \
+           (struct sockaddr *)&servaddr, sizeof(servaddr));
 
-	   /* free memory */
-	   free(jsonevent);
-	   free(jsoneventstring);
+       /* free memory */
+           json_decref(jsonevent);
+       free(jsoneventstring);
       }
     } /* loop forever, reading from a file */
 }
